62e7b1b25520cee8ff8aa6781ff2276bd6ed42ef
mak20th <mak20th@gmail.com>
2019-02-22 14:49:37 -0600

    added files

---
+++ index.js
@@ -0,0 +1,8 @@
+//you need to write JavaScript that provides the following experience: when a user selects one of the thumbnail images 
+//using the mouse or keyboard, that image should be displayed in the full-size image container at the top. Updating the 
+//image should also update its alt attribute. To test keyboard interactivity, you can Tab to one of the thumbnails and press the Enter key.
+
+'use strict';
+$(function(){
+    
+})
