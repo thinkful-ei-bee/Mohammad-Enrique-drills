fca56fb658e03c264e4b90dfc7a4d1c8bd744018
mak20th <mak20th@gmail.com>
2019-02-23 21:14:33 -0600

    added  cat carousel

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //you need to write JavaScript that provides the following experience: when a user selects one of the thumbnail images 
 //using the mouse or keyboard, that image should be displayed in the full-size image container at the top. Updating the 
 //image should also update its alt attribute. To test keyboard interactivity, you can Tab to one of the thumbnails and press the Enter key.
+
 /*global $ */
 'use strict';
 $(function(){
+  $('.thumbnail').on('click', function() {
+    var clickedImage = $(this).find('img').clone();
+    var heroImage = $('.hero').find('img');
     
+    heroImage.replaceWith(clickedImage);
+
+    // heroImage.prop('src', clickedImage.prop('src'));
+    // heroImage.prop('alt', clickedImage.prop('alt'));
+  });
+
 });
